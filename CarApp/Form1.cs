7042e2caa5f8dd63bf34c0368b67c0834d15b872
Theo Holmgren <theokodar@gmail.com>
2021-05-12 09:13:25 +0200

    Avslutade koden

--- CarApp/Form1.cs
+++ CarApp/Form1.cs
@@ -70,6 +70,17 @@ namespace CarApp
 
         private void btnRemove_Click(object sender, EventArgs e)
         {
+            int res = dbObject.RemoveCarRowByRegNr(lsvCars.SelectedItems[0].Text);
+            MessageBox.Show("Du har tagit bort " + Convert.ToString(res) + " antal bilar från databasen");
+            if (res > 0)
+            {
+                RemoveCarFromListView(lsvCars.SelectedItems[0]);
+            }
+   
+        }
+
+        private void RemoveCarFromListView(ListViewItem listViewItem)
+        {
             if (lsvCars.SelectedItems.Count > 0)
             {
                 var item = lsvCars.SelectedItems[0];
@@ -82,7 +93,6 @@ namespace CarApp
             }
             btnClear.Enabled = (lsvCars.Items.Count > 0);
         }
-
         private void lsvCars_SelectedIndexChanged(object sender, EventArgs e)
         {
             btnRemove.Enabled = (lsvCars.SelectedItems.Count > 0);
@@ -90,8 +100,67 @@ namespace CarApp
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-            lsvCars.Items.Clear();
+            int res = dbObject.RemoveAllCars();
+            if ( res == lsvCars.Items.Count)
+            {
+                lsvCars.Items.Clear();
+            }
+            else
+            {
+                lsvCars.Items.Clear();
+                InitListView();
+            }
             txtRegNr.Focus();
+        }
+        /// <summary>
+        /// PrintData - Hämtar data om regNr från bilregistret och fyller i de textbox:arna för Märke, Model och År
+        /// </summary>
+        /// <param name="regNr"> Bilens registreringsnummer med stora bokstäver (Ex "HKM788")</param>
+        private void PrintData(string regNr) //gamla Printdata
+        {
+            // Exempel https://api.biluppgifter.se/api/v1/vehicle/regno/XNF905?api_token=ZYdERdMQ1BLgQ9DP6hwZpO7ScLeXcJUm
+
+
+
+            string token = "ZYdERdMQ1BLgQ9DP6hwZpO7ScLeXcJUm";
+            string call = String.Format($"https://api.biluppgifter.se/api/v1/vehicle/regno/{regNr}?api_token={token}");
+
+
+
+            try
+            {
+                // Skapa objekt för att skicka en fråga till bilregistret
+                WebRequest request = HttpWebRequest.Create(call);
+
+
+
+                // Skapa ett svarsobjekt
+                WebResponse response = request.GetResponse();
+
+
+
+                // Läs av "the stream" som är i responsen
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+
+
+                string carJSON = reader.ReadToEnd();
+
+                JObject jsonCar = JObject.Parse(carJSON);
+
+
+
+                txtMake.Text = jsonCar["data"]["basic"]["data"]["make"].ToString();
+                txtModel.Text = jsonCar["data"]["basic"]["data"]["model"].ToString();
+                txtYear.Text = jsonCar["data"]["basic"]["data"]["model_year"].ToString();
+
+
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Bil med registreringsnummer {regNr} kunde inte hittas\n\nMeddelande: {e.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
         private ListViewItem CreateListViewItem(Car car)
         {
@@ -100,7 +169,7 @@ namespace CarApp
             item.SubItems.Add(car.GetModel());
             item.SubItems.Add(car.GetYear().ToString());
             item.SubItems.Add(car.GetForSale() ? "Yes" : "No");
-
+            return item;
             
         }
         private void ClearTextboxes()
