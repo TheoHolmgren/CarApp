8b38b34a20765cc8010aec25671fee9cd812765d
Theo Holmgren <theokodar@gmail.com>
2021-05-26 09:12:51 +0200

    Fixade GUI

--- CarApp/Form1.Designer.cs
+++ CarApp/Form1.Designer.cs
@@ -31,8 +31,11 @@
             this.lsvCars = new System.Windows.Forms.ListView();
             this.regnr = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.make = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.model = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.year = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.forsale = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSearch = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
             this.cbxForSale = new System.Windows.Forms.CheckBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -42,12 +45,9 @@
             this.txtYear = new System.Windows.Forms.TextBox();
             this.txtModel = new System.Windows.Forms.TextBox();
             this.txtMake = new System.Windows.Forms.TextBox();
-            this.txtRegNr = new System.Windows.Forms.TextBox();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnRemove = new System.Windows.Forms.Button();
-            this.btnSearch = new System.Windows.Forms.Button();
-            this.model = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.year = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtRegNr = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,6 +59,7 @@
             this.model,
             this.year,
             this.forsale});
+            this.lsvCars.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lsvCars.Location = new System.Drawing.Point(23, 196);
             this.lsvCars.Name = "lsvCars";
             this.lsvCars.Size = new System.Drawing.Size(853, 302);
@@ -77,6 +78,14 @@
             this.make.Text = "Märke";
             this.make.Width = 83;
             // 
+            // model
+            // 
+            this.model.Text = "Model";
+            // 
+            // year
+            // 
+            this.year.Text = "Year";
+            // 
             // forsale
             // 
             this.forsale.Text = "Till Salu";
@@ -84,6 +93,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.txtRegNr);
             this.groupBox1.Controls.Add(this.btnSearch);
             this.groupBox1.Controls.Add(this.btnAdd);
             this.groupBox1.Controls.Add(this.cbxForSale);
@@ -94,29 +104,43 @@
             this.groupBox1.Controls.Add(this.txtYear);
             this.groupBox1.Controls.Add(this.txtModel);
             this.groupBox1.Controls.Add(this.txtMake);
-            this.groupBox1.Controls.Add(this.txtRegNr);
             this.groupBox1.Location = new System.Drawing.Point(23, 16);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(839, 159);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnSearch.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(100, 118);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(175, 35);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Sök i biluppgifter.se";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
             // btnAdd
             // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnAdd.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAdd.Location = new System.Drawing.Point(697, 72);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(104, 36);
             this.btnAdd.TabIndex = 9;
             this.btnAdd.Text = "Lägg till";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // cbxForSale
             // 
             this.cbxForSale.AutoSize = true;
+            this.cbxForSale.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxForSale.Location = new System.Drawing.Point(697, 27);
             this.cbxForSale.Name = "cbxForSale";
-            this.cbxForSale.Size = new System.Drawing.Size(89, 24);
+            this.cbxForSale.Size = new System.Drawing.Size(100, 24);
             this.cbxForSale.TabIndex = 8;
             this.cbxForSale.Text = "Till Salu";
             this.cbxForSale.UseVisualStyleBackColor = true;
@@ -124,36 +148,40 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(346, 77);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.Size = new System.Drawing.Size(27, 20);
             this.label4.TabIndex = 7;
             this.label4.Text = "År";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(346, 28);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.Size = new System.Drawing.Size(62, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Modell";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(23, 77);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.Size = new System.Drawing.Size(59, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "Märke";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(23, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.Size = new System.Drawing.Size(63, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "Reg Nr";
             // 
@@ -178,60 +206,50 @@
             this.txtMake.Size = new System.Drawing.Size(175, 26);
             this.txtMake.TabIndex = 1;
             // 
-            // txtRegNr
-            // 
-            this.txtRegNr.Location = new System.Drawing.Point(100, 25);
-            this.txtRegNr.Name = "txtRegNr";
-            this.txtRegNr.Size = new System.Drawing.Size(175, 26);
-            this.txtRegNr.TabIndex = 0;
-            // 
             // btnClear
             // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnClear.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClear.Location = new System.Drawing.Point(23, 504);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(184, 30);
             this.btnClear.TabIndex = 2;
             this.btnClear.Text = "Rensa bort all data";
-            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.UseVisualStyleBackColor = false;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnRemove
             // 
+            this.btnRemove.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnRemove.Font = new System.Drawing.Font("Bookman Old Style", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRemove.Location = new System.Drawing.Point(684, 504);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(178, 30);
             this.btnRemove.TabIndex = 3;
             this.btnRemove.Text = "Ta bort markerad";
-            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.UseVisualStyleBackColor = false;
             this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
-            // btnSearch
+            // txtRegNr
             // 
-            this.btnSearch.Location = new System.Drawing.Point(100, 118);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(175, 35);
-            this.btnSearch.TabIndex = 10;
-            this.btnSearch.Text = "Sök i biluppgifter.se";
-            this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
-            // 
-            // model
-            // 
-            this.model.Text = "Model";
-            // 
-            // year
-            // 
-            this.year.Text = "Year";
+            this.txtRegNr.Location = new System.Drawing.Point(100, 25);
+            this.txtRegNr.Name = "txtRegNr";
+            this.txtRegNr.Size = new System.Drawing.Size(175, 26);
+            this.txtRegNr.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(888, 546);
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(887, 557);
             this.Controls.Add(this.btnRemove);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lsvCars);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
@@ -255,13 +273,13 @@
         private System.Windows.Forms.TextBox txtYear;
         private System.Windows.Forms.TextBox txtModel;
         private System.Windows.Forms.TextBox txtMake;
-        private System.Windows.Forms.TextBox txtRegNr;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnRemove;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.ColumnHeader model;
         private System.Windows.Forms.ColumnHeader year;
+        private System.Windows.Forms.TextBox txtRegNr;
     }
 }
 
